415f85f41eaee97e73970fc8208320cc63ee38e4
Andre Hollow <kinomaru@mail.com>
2018-12-06 20:08:43 +0100

    Add files via upload

---
+++ docs/precache-manifest.5ccd828d4efbd5df325eb999e0977369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14f4b90203dc4a0161e7d12788befd50",
+    "url": "/ReactTodo/static/media/yin-yang1.14f4b902.svg"
+  },
+  {
+    "revision": "5deac06e4f762c64d1a9",
+    "url": "/ReactTodo/static/js/runtime~main.5deac06e.js"
+  },
+  {
+    "revision": "ee221e9bd74422cf2316",
+    "url": "/ReactTodo/static/js/main.ee221e9b.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/ReactTodo/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "ee221e9bd74422cf2316",
+    "url": "/ReactTodo/static/css/main.b4a5feb4.chunk.css"
+  },
+  {
+    "revision": "6fbe3206ba250e2898e3a6bb0c3c6a45",
+    "url": "/ReactTodo/index.html"
+  }
+];
